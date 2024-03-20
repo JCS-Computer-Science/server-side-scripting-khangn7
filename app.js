f9e2672734ea4f0ad9860fa44f614c4390af7003
Khang Ngo <1100057805@edu.cbe.ab.ca>
2024-03-20 15:26:20 -0600

    asdf

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require("express");
 
+const fs = require('fs')
+const path = require("path")
+
 const app = express();
 //All your code goes here
 
 // https://stackoverflow.com/questions/7172784/how-do-i-post-json-data-with-curl
+// note: res.send doesn't stop function
 
 app.use(express.static('public'))
 app.use(express.json())
 
-const fs = require('fs');
-const path = require("path");
 
 // https://stackoverflow.com/questions/18386361/read-a-file-in-node-js
-const COURSES_PATH = path.join(__dirname, "/database/courses.json");
-const courses = JSON.parse(fs.readFileSync(COURSES_PATH, 'utf8'));
-const USERS_PATH = path.join(__dirname, "/database/users.json");
-const users = JSON.parse(fs.readFileSync(USERS_PATH, "utf-8"));
+const COURSES_PATH = path.join(__dirname, "/database/courses.json")
+const courses = JSON.parse(fs.readFileSync(COURSES_PATH, 'utf8'))
+const USERS_PATH = path.join(__dirname, "/database/users.json")
+const users = JSON.parse(fs.readFileSync(USERS_PATH, "utf-8"))
 
 class User {
     constructor(username, password, id) {
@@ -27,7 +29,21 @@ class User {
 }
 
 app.get('/courses', async (req, res) => {
-    res.send(courses)
+    let code = req.query.code;
+    let num = req.query.num;
+    let requested;
+    if (code && num) {
+        requested = courses.filter((course) => { 
+            return (course.code === code) && course.num.startsWith(num)
+        });
+    } else if (code) {
+        requested = courses.filter((course) => course.code === code);
+    } else if (num) {
+        requested = courses.filter((course) => course.num.startsWith(num));
+    } else {
+        requested = courses
+    }
+    res.send(requested)
 });
 
 // returns false if doesn't exist
@@ -35,7 +51,7 @@ function findUser(id, username, password) {
     if (id) {
         for (let obj of users) {
             if (obj.id === id) {
-                return obj
+                return obj // reference
             }
         }
     } else {
@@ -45,11 +61,12 @@ function findUser(id, username, password) {
             }
         }
     }
-
+    return false
 }
 
 app.get("/account/:id", (req, res) => {
-    let user = findUser(req.params.id)
+    let user = findUser(Number(req.params.id))
+    console.log(user);
     if (!user) {
         user = {"error": "id doesn't exist"}
     }
@@ -68,17 +85,65 @@ app.post("/users/login", (req, res) => {
 
 app.post("/users/signup", (req, res) => {
     let username = req.body.username,
-        password = req.body.password;
-    let userId = findUser(false, username, password);
-    let response;
+        password = req.body.password
+    let userId = findUser(false, username, password)
+    let response
     if (userId) {
-        response = {"error": "username already in use"};
+        response = {"error": "username already in use"}
     } else {
-        let newId = users.length + 1;
-        let newUser = new User(username)
+        let newId = users.length + 1
+        let newUser = new User(username, password, newId)
+        users.push(newUser)
+        fs.writeFileSync(USERS_PATH, JSON.stringify(users, null, 4), "utf-8")
+        response = {"userId": newId}
     }
+    res.send(response);
 })
 
+app.patch("/account/:id/courses/add", (req, res) => {
+    // check if valid id
+    let user = findUser(Number(req.params.id))
+    if (!user) {
+        res.send({"error": "id doesn't exist"})
+        return
+    }
+    
+    // check if course exists
+    let reqcourse = req.body.course
+    // console.log(reqcourse);
+    let course_to_add = false
+    for (let course of courses) {
+        if (course.code === reqcourse.code &&
+            course.num === reqcourse.num &&
+            course.name === reqcourse.name)
+        {
+            course_to_add = course
+            break
+        }
+    }
+    if (!course_to_add) {
+        res.send({"error": "course doesn't exist"})
+        return
+    }
+
+    // check if already in users course list
+    for (c of user.courses) {
+        if (c.code === course_to_add.code &&
+            c.num === course_to_add.num &&
+            c.name === course_to_add.name) {
+            res.send({"error": "course already in user list"})
+            return
+        }
+    }
+
+    // else add to user courses
+    // note that user is reference to obj in users
+    user.courses.push(JSON.parse(JSON.stringify(course_to_add)))
+    fs.writeFileSync(USERS_PATH, JSON.stringify(users, null, 4), "utf-8")
+
+    res.send(user.courses)
+    
+});
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
